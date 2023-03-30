72abc23bd419474c51e46c62212482b36fd0d054
sokka1000 <catalin_ionita04@yahoo.com>
2023-03-30 21:08:02 +0300

    create the project

---
+++ src/main/java/com/example/recipenowwebappbackend/RecipeNowWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipenowwebappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeNowWebAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeNowWebAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipenowwebappbackend/RecipeNowWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipenowwebappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeNowWebAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
